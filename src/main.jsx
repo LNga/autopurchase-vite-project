13b5a9a6455e04fde7a1988abeb4c42d08e9551b
Liz NDG <liz.ndouga@gmail.com>
2022-10-10 11:42:19 +0200

    adding router dom in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <App />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
