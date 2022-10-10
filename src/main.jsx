62a0f89ab29e7943bc8529abac343bb8078e2e73
Liz NDG <liz.ndouga@gmail.com>
2022-10-10 12:15:38 +0200

    removing browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/autopurchase-vite-project/',
+  base: 'autopurchase-vite-project',
   plugins: [react()],
 });
