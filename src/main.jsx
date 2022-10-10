62cdd4f7fb4d39f5ba5e4effb4681fb6a0ae1723
Liz NDG <liz.ndouga@gmail.com>
2022-10-10 11:44:24 +0200

    removing switch

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Switch>
-        <Route exact path='/'>
-          <App />
-        </Route>
-      </Switch>
+      <Route exact path='/'>
+        <App />
+      </Route>
     </BrowserRouter>
   </React.StrictMode>
 );
