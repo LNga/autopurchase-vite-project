78a2e94d81a16b9801ed317b84194a1d411a0650
Liz NDG <liz.ndouga@gmail.com>
2022-10-10 11:12:48 +0200

    removing public and assets folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png'
+          className='App-logo'
+          alt='logo'
+        />
+        <p>Deploy to GitHub Pages</p>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <button type='button' onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
